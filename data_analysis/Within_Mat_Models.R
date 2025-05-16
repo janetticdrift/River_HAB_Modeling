63bc84f2817436845de4777734a8a08cf5dbb14a
janetticdrift <jdavid21@uwyo.edu>
2025-05-16 15:41:47 -0700

    Removed "empty" cells from qPCR_2024 dataset. Create plots to look at the relationship between gene copy and DNA concentration

--- data_analysis/Within_Mat_Models.R
+++ data_analysis/Within_Mat_Models.R
@@ -2,8 +2,33 @@
 library(tidyverse)
 
 withinmat <- read.csv(here::here("data/16S_2024.csv")) #2024 data
+genecopy <- read.csv(here::here("data/qPCR_2024.csv")) #2024 data
 
-#Must remove the percentages in percent_comp
+#Clean data: Must remove the percentages in percent_comp
 withinmat <- withinmat %>% 
   mutate(across(everything(), str_remove_all, "%"))
 withinmat$percent_comp <- as.numeric(as.character(withinmat$percent_comp))
+
+library(plotly)
+library(ggplot2)
+
+
+p1 <- ggplot(genecopy, aes(x = DNA_conc, y = ana_C.uL, color = mat, label = sample)) +
+  geom_point() +
+  coord_cartesian(ylim = c(0, 2000)) +
+  labs(title = "anaC Gene Copies", x = "DNA concentration ng/uL", 
+       y = "anaC Copy Count")
+
+p2 <- ggplot(genecopy, aes(x = DNA_conc, y = nif.uL, color = mat, label = sample)) +
+  geom_point() +
+  coord_cartesian(ylim = c(0, 2000)) +
+  labs(title = "nif Gene Copies", x = "DNA concentration ng/uL", 
+       y = "nif Copy Count")
+
+ggarrange(
+  p1, p2, labels = c("A", "B"),
+  common.legend = TRUE, legend = "bottom"
+)
+
+
+ggplotly(plotgenecopy, tooltip = "sample")
