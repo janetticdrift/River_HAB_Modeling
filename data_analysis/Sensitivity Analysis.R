b491a3aa892a73470404454eb6d21c56dc8abee0
janetticdrift <jdavid21@uwyo.edu>
2026-02-11 02:28:35 -0800

    Created plots comparing gene copies, gene copies with anatoxin amounts, and nif with epithemia. Created sensitivity model, created plots. Issues with exploding abundances

--- data_analysis/Sensitivity Analysis.R
+++ data_analysis/Sensitivity Analysis.R
@@ -17,28 +17,75 @@ Dtheta <- x[["Dtheta"]][,]
 Ttheta <- x[["Ttheta"]][,]
 Ctheta <- x[["Ctheta"]][,]
 Rtheta <- x[["Rtheta"]][,]
+#Combine env theta effects into a list
+theta_list <- list(
+  N = Ntheta,
+  P = Ptheta,
+  A = Atheta,
+  D = Dtheta,
+  Tt = Ttheta,
+  C = Ctheta,
+  R = Rtheta
+)
 
 #Create identity matrix
-ID <- diag(1, nrow = 4, ncol = 4) #4 is the number of species in the river-wide model
+ID <- diag(1, nrow = 4, ncol = 4) # 4 is the number of species in the river-wide model
 
-#Find equilibrium abundances
-#Equi_abund_W = A / (Identity matrix - B)
+#Set conditions and output storage
+env_range <- seq(-3, 3, by = 0.5)
+env_names <- names(theta_list)
 
-for(z in 1:runs){
-  #Set parameters
+w_star <- array(        # iteration, env_var, perturbation, species
+  NA, 
+  dim = c(15000, length(env_names), length(env_range), 4),
+  dimnames = list(
+    iteration = NULL,
+    env = env_names,
+    env_peturb = env_range,
+    species = paste0("sp", 1:4)
+  )
+)
+
+#Run model
+for(z in 1:15000){
+  
   Beta <- betas[z,,]
   
-  #Pull env covariates
-  nTheta <- Ntheta[z,]
-  pTheta <- Ptheta[z,]
-  aTheta <- Atheta[z,]
-  dTheta <- Dtheta[z,]
-  tTheta <- Ttheta[z,]
-  cTheta <- Ctheta[z,]
-  rTheta <- Rtheta[z,]
+  #Include species interactions
+  M_inverse <- solve(ID - Beta)    #solve() takes the inverse of matrices
   
-  #Model
-  w[,,z] <- nTheta / (ID - Beta)
-    n[t,,z] <- MASS::mvrnorm(n = 1, mu = Alpha + Beta%*%n[t-1,,z],
-                             Sigma = sigma)
+  #Exclude species interactions
+  # B_intra <- Beta
+  # B_intra[row(B_intra) != col(B_intra)] <- 0    #any time row doesn't equal column, set to 0
+  # M_inv_intra <- solve(ID - B_intra)
+  
+  for(v in seq_along(env_names)){     #Use seq_along for iterating along characters
+    
+    theta_matrix <- theta_list[[v]]   #Pull out current env var of interest
+    theta_vec <- theta_matrix[z,]     #Pull out theta vector of z iteration
+    
+    for(e in seq_along(env_range)){
+      
+      A <- theta_vec * env_range[e]
+      w_star[z, v, e, ] <- as.vector(M_inverse %*% A) #Switch between M_inverse and M_inv_intra
+      
+    }
+  }
 }
+
+#Clean output for plotting
+eq_abund <- as.data.frame.table(w_star, responseName = "w_star") %>%
+  mutate(
+    env_peturb = as.numeric(as.character(env_peturb)),
+    iteration = as.integer(iteration),
+    species = factor(species), # Green algae, Microcoleus, Anabaena, Other N-fixers
+    env = factor(env)
+  )
+
+#Plot outputs
+  #Species2: Microcoleus
+ggplot(subset(eq_abund, species %in% "sp2"), aes(x = factor(env_peturb), y = w_star)) +
+  facet_wrap(~env, scales = "free") +
+  geom_boxplot(outliers = T)
+
+
--- data_cleaning/Toxin Models.R
+++ data_cleaning/Toxin Models.R
@@ -8,23 +8,120 @@
 library(rstan)
 library(tidyverse)
 library(dataRetrieval)
+library(ggplot2)
+library(ggpubr)
+library(RColorBrewer)
 
 #First examine relationships between anaC genes and anatoxin quantities
+pseudocount <- 1e-4 #Set small number for 0s, to not return error when log-transformed
+
 pcr_atx <- read.csv(here::here("data/qPCR_Anatoxins.csv")) %>% 
   filter(!grepl("analysis", Notes)) %>%  #Remove samples not for analysis ("floating" and "gravel")
+  rename(field_date = date) %>% 
+  mutate(field_date = as.Date(field_date, format="%m/%d/%y")) %>%  #Fix date data type
+  mutate(
+    # Normalized anaC gene copies (copies per ng DNA)
+    normalized_anaC = ana_C.uL / DNA_conc_ng_uL,
+    normalized_anaC_rerun = ana_C.uL_rerun / DNA_conc_rerun,
+    # Normalized nif gene copies (copies per ng DNA)
+    normalized_nif = nif.uL / DNA_conc_ng_uL,
+    normalized_nif_rerun = nif.uL_rerun / DNA_conc_rerun,
+    
+    # Log10 normalized values
+    across(c(normalized_anaC, normalized_anaC_rerun,
+             normalized_nif, normalized_nif_rerun),
+           ~ log10(.x + pseudocount))
+  ) %>% 
+  pivot_longer(
+    cols = c(normalized_anaC, normalized_anaC_rerun,
+             normalized_nif, normalized_nif_rerun),
+    names_to = "gene_run",
+    values_to = "value"
+  ) %>%
+  mutate(
+    gene = case_when(
+      str_detect(gene_run, "anaC") ~ "anaC",
+      str_detect(gene_run, "nif")  ~ "nif"
+    ),
+    run_type = if_else(str_detect(gene_run, "rerun"), "rerun", "original")
+  ) %>%
+  select(-gene_run) %>% 
+  pivot_wider(names_from = gene, values_from = value) %>% 
+  mutate(ATX_all_ug_L = ATX_all_ug_g * 1000)  #Transform ATX units from ug/g to ug/L
   
+
+#ana_C vs nif, exclude zeros
+ggplot(pcr_atx, aes(x = anaC, y = nif, color = mat)) +
+  facet_wrap(~year, scales = "free") +
+  geom_point(aes(shape = run_type), size = 2) +
+  geom_smooth(data = . %>% filter(anaC != -4 & nif != -4), # filter out 0 values
+    method = "lm", se = FALSE) +
+  stat_regline_equation(
+    data = . %>% filter(anaC != -4 & nif != -4),
+    aes(label = paste(..rr.label..)),
+    formula = y ~ x,
+    parse = TRUE,
+    label.x.npc = "left",
+    label.y.npc = "top") +
+  labs(x = "anaC gene (copies/ng)", y = "nif gene (copies/ng)") +
+  theme_bw()
+
+#ana_C vs nif, include zeros
+ggplot(pcr_atx, aes(x = anaC, y = nif, color = mat)) +
+  facet_wrap(~year, scales = "free") +
+  geom_point(aes(shape = run_type), size = 2) +
+  geom_smooth(method = "lm", se = FALSE) +
+  stat_regline_equation(
+     aes(label = paste(..rr.label..)),
+     formula = y ~ x,
+     parse = TRUE,
+     label.x.npc = "left",
+     label.y.npc = "top") +
+  labs(x = "anaC gene (copies/ng)", y = "nif gene (copies/ng)") +
+  theme_bw()
+
+#ana_C vs Anatoxins, include zeros
+ggplot(pcr_atx, aes(x = anaC, y = ATX_all_ug_L, color = mat)) +
+  facet_wrap(~year, scales = "free") +
+  geom_point( size = 2) +
+  geom_smooth(data = . %>% filter(anaC != -4 & ATX_all_ug_L != 0),
+              method = "lm", se = FALSE) +
+  stat_regline_equation(
+    data = . %>% filter(anaC != -4 & ATX_all_ug_L != 0),
+    aes(label = paste(..rr.label..)),
+    formula = y ~ x,
+    label.x.npc = "left",
+    label.y.npc = "top") +
+  labs(x = "anaC gene (copies/ng)", y = "Total anatoxin (ug/L)") +
+  scale_colour_brewer(type = "qual") +
+  theme_bw()
+
+
+
+
+#Compare nif presence with epithemia
+nif_ediatoms <- microscopy %>%
+  rename(mat = sample_type) %>% 
+  mutate(mat = case_when(
+    mat == "TAC" ~ "Anabaena",
+    mat == "TM" ~ "Microcoleus")) %>% 
+  inner_join(pcr_atx, by = c("field_date", "year", "mat")) %>%
+  select(field_date, nif, e_diatoms, year, mat)
+
+ggplot(nif_ediatoms, aes(x = nif, y = e_diatoms, color = mat)) +
+  facet_wrap(~year, scales = "free") +
+  geom_point() +
+  geom_smooth(data = . %>% filter(nif != -4 & e_diatoms != 0),
+              method = "lm", se = FALSE) +
+  stat_regline_equation(
+    data = . %>% filter(nif != -4 & e_diatoms != 0),
+    aes(label = paste(..rr.label..)),
+    formula = y ~ x,
+    label.x.npc = "left",
+    label.y.npc = "top") +
+  scale_colour_brewer(palette = "Dark2")
   
-#ana_C and nif
-ggplot(pcr_atx, aes(x = ana_C.uL, ana_C.uL_rerun, y = nif.uL, nif.uL_rerun)) +
-  facet_wrap(~year, scales = "free") +
-  geom_point(aes(color = mat))
 
-ggplot(subset(pcr_atx, mat  %in% "Anabaena"), aes(x = ana_C.uL_rerun, y = ATX_all_ug_g)) +
-  facet_wrap(~year, scales = "free") +
-  geom_jitter(aes(color = mat))
-
-ggplot(params2_all, aes(x = model_date, y = mean)) + 
-  facet_wrap(~year, scales = "free")
 #Read in environmental and microscopy data
 source(here::here("data_cleaning/cleaning_HAB.R"))
 #Read in anatoxin data
