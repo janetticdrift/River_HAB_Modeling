aec6001591977ffb0723f0eb52bccd523fbf50f0
janetticdrift <jdavid21@uwyo.edu>
2026-02-17 10:12:32 -0800

    Add new file of cleaned data to send to coauthor for code tinkering

---
+++ data_analysis/Compare qPCR ATX.R
@@ -0,0 +1,107 @@
+############################################
+###Anatoxin Data Cleaning and Exploration###
+############################################
+
+#Packages----
+library(tidyverse)
+library(ggplot2)
+library(ggpubr)
+library(RColorBrewer)
+
+#Read in datasheet and clean
+
+pseudocount <- 1e-4 #Set a small number for 0s, to not return error when log-transformed
+
+pcr_atx <- read.csv(here::here("data/qPCR_Anatoxins_forGoel.csv")) %>% 
+  filter(!grepl("analysis", UNR_Notes)) %>%  #Remove samples noted as not for analysis ("floating" and "gravel")
+  rename(field_date = date) %>% 
+  mutate(field_date = as.Date(field_date, format="%m/%d/%y")) %>%  # Fix date data type
+  mutate(
+    across(c(ana_C.uL, ana_C.uL_rerun,
+             nif.uL, nif.uL_rerun),
+           ~ log10(.x + pseudocount))  #Add small pseudocount to all numbers, then transform
+  ) %>% 
+  pivot_longer(   #Pivot dataframe to create a group for run or rerun in the next function
+    cols = c(ana_C.uL, ana_C.uL_rerun,
+             nif.uL, nif.uL_rerun),
+    names_to = "gene_run",
+    values_to = "value"
+  ) %>%
+  mutate(
+    gene = case_when(                     #Remove extra characters from gene name
+      str_detect(gene_run, "ana_C") ~ "anaC",
+      str_detect(gene_run, "nif")  ~ "nif"
+    ),
+    run_type = if_else(str_detect(gene_run, "rerun"), "rerun", "original") #Create run type grouping
+  ) %>%
+  select(-gene_run) %>% 
+  pivot_wider(names_from = gene, values_from = value) %>% 
+  mutate(
+    across(c(ATX_all_ug_g, ATXa_ug_g,    #Set any "bdl" ATX values to 0 to be plotted
+             HTXa_ug_g, dhATXa_ug_g),
+           ~ case_when(. == "bdl" ~ '0',
+                       . != "bdl" ~ .))
+  ) %>% 
+  mutate(
+    across(c(ATX_all_ug_g, ATXa_ug_g,    #Convert ATX values from characters to numeric
+             HTXa_ug_g, dhATXa_ug_g),
+           ~ as.numeric(.))
+  )
+
+
+
+#------------------------------------------------------------------------------
+################
+#GRAPHING GENES#
+################
+
+#nif ~ anaC, exclude zeros from regression line
+ggplot(pcr_atx, aes(x = anaC, y = nif, color = mat)) +
+  facet_wrap(~year, scales = "free") +  #Split plot between the two years, scales = "free" means each year has its own Y axis 
+  geom_point(aes(shape = run_type), size = 2) +
+  geom_smooth(data = . %>% filter(anaC != -4 & nif != -4), # filter out "0" values
+              method = "lm", se = FALSE) +
+  stat_regline_equation(                  #Calculate r^2 values
+    data = . %>% filter(anaC != -4 & nif != -4),   #Remove "0" values from calculating r^2
+    aes(label = paste(..rr.label..)),
+    formula = y ~ x,
+    parse = TRUE,
+    label.x.npc = "left",
+    label.y.npc = "top") +
+  labs(x = "anaC gene (copies/ng)", y = "nif gene (copies/ng)") +
+  theme_bw()
+
+#nif ~ anaC, include zeros in regression line
+ggplot(pcr_atx, aes(x = anaC, y = nif, color = mat)) +
+  facet_wrap(~year, scales = "free") +
+  geom_point(aes(shape = run_type), size = 2) +
+  geom_smooth(method = "lm", se = FALSE) +
+  stat_regline_equation(
+    aes(label = paste(..rr.label..)),
+    formula = y ~ x,
+    parse = TRUE,
+    label.x.npc = "left",
+    label.y.npc = "top") +
+  labs(x = "anaC gene (copies/ng)", y = "nif gene (copies/ng)") +
+  theme_bw()
+
+##############
+#GRAPHING ATX#
+##############
+
+#ana_C vs Anatoxins, exclude zeros from regression line
+ggplot(pcr_atx, aes(x = anaC, y = log(ATX_all_ug_g), color = mat)) + #ATXs log-transformed
+  facet_wrap(~year, scales = "free") +
+  geom_point( size = 2) +
+  geom_smooth(data = . %>% filter(anaC != -4 & ATX_all_ug_g != 0),
+              method = "lm", se = FALSE) +
+  stat_regline_equation(
+    data = . %>% filter(anaC != -4 & ATX_all_ug_g != 0),
+    aes(label = paste(..rr.label..)),
+    formula = y ~ x,
+    label.x.npc = "left",
+    label.y.npc = "top") +
+  labs(x = "anaC gene (copies/ng)", y = "Total anatoxin (ug/g)") +
+  scale_colour_brewer(type = "qual") +    #Use different colors for anatoxin plot
+  theme_bw()
+
