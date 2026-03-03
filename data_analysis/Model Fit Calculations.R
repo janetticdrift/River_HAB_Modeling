49cb2e769b78c11e9be1115f8643ae7f6f0fbf6e
janetticdrift <jdavid21@uwyo.edu>
2026-03-02 20:42:53 -0800

    Created new model fit code for observed vs latent microscopy data

--- data_analysis/Model Fit Calculations.R
+++ data_analysis/Model Fit Calculations.R
@@ -100,36 +100,34 @@ apply(R2, 2, quantile, c(0.025, 0.975))
 #WITHIN MAT
 #OBSERVED DATA VS POSTERIORS
 ###############------------------------------------------------------------------
-#Extract the relevant model here. fit.m4 = all variables,
-#fit.m5 = biotic interactions, and fit.m6 = abiotic effects.
-posteriors <- rstan::extract(fit.m6)[["n"]] #array indexed by iterations, species #, time
+#Extract the relevant model here. fit.m1 = microcoleus, fit.m2 = anabaena.
+posteriors_mat <- rstan::extract(fit.m1)[["n"]] #array indexed by iterations, species #, time
 
 ###############------------------------------------------------------------------
 #Extract observed data vectors from alltaxatime, raw data object
-y_micro <- alltaxatime$microcoleus
-y_ana <- alltaxatime$anabaena_cylindrospermum
-y_green <- alltaxatime$green_algae
-y_nfix <- alltaxatime$other_nfixers
+y_ana <- matalltaxaM$Anabaena
+y_epi <- matalltaxaM$`Epithemia Diatoms`
+y_geit <- matalltaxaM$Geitlerinema
 
 #Put them into a single matrix
-y_obs_river <- rbind(y_green, y_micro, y_ana, y_nfix) #Dimensions: Species, time
+y_obs_mat <- rbind(y_ana, y_epi, y_geit) #Dimensions: Species, time
 
 ###############------------------------------------------------------------------
 #Bayesian R2 comparing latent vs observed states
-iter <- dim(posteriors)[1]  # Number of iterations
-species <- dim(posteriors)[2]  # Number of species
-time <- dim(posteriors)[3]  # Time steps
+iter <- dim(posteriors_mat)[1]  # Number of iterations
+species <- dim(posteriors_mat)[2]  # Number of species
+time <- dim(posteriors_mat)[3]  # Time steps
 
 R2 <- matrix(NA, iter, species) #Create empty matrix for R2 values per iteration, per species
 
 for (s in 1:species) {
   
-  y_obs <- y_obs_river[s, ]
+  y_obs <- y_obs_mat[s, ]
   obs_index <- which(y_obs != -99) #Remove weeks where we did not collect field data
   
   for (i in 1:iter) {
     
-    yhat <- posteriors[i, s, obs_index] #In the time index, take out weeks with no field data
+    yhat <- posteriors_mat[i, s, obs_index] #In the time index, take out weeks with no field data
     yobs <- y_obs[obs_index]
     
     var_fit <- var(yhat)
